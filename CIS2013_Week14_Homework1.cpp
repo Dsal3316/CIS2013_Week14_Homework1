41c43e94cfdc5c0e7c8552a0341fbfb785a0af6f
Dsal3316 <35541000+Dsal3316@users.noreply.github.com>
2018-05-01 19:40:37 -0500

    commit

--- CIS2013_Week14_Homework1.cpp
+++ CIS2013_Week14_Homework1.cpp
@@ -61,6 +61,75 @@ class minesweeper: public game{
 				}
 				selected_squares[i]=t;
 			}
+			
+			bomb_sq=new int[mines_count * 2];
+			int mx,my;
+			for (int n=0; n<mines_count * 2; n+=2)
+			{
+				mx=rand()%x;
+				my=rand()%y;
+				
+				while (1)
+				{
+					int i;
+					for (i=0; i<n; i+=2)
+					{
+						if(bomb_sq[i] == mx && bomb_sq[i+1] ==my)
+						{
+							mx=rand()%x;
+							mx=rand()%y;
+							break;
+						}
+					}
+					if(i==n)
+						break;
+				}
+				bomb_sq[n]=mx;
+				bomb_sq[n+1]=my;
+			}
+		}
+		
+		void print(){
+			
+			cout<< " ";
+			for (int a=0; a < x; a++)
+			{
+				cout<< a << ' ';
+			}
+			cout<< endl;
+			
+			for (int i=0; i<y; i++)
+			{
+				cout<< i << ' ';
+				for (int j=0; j<x; j++)
+				{
+					cout << selected_squares[i][j] << ' ';
+				}
+				cout<< endl;
+			}
+			cout<< endl;
+		}
+		
+		bool get_sqaure(int x, int y)
+		{
+			int n;
+			for (n=0; n< mines_amount*2; n += 2)
+			{
+				if (bomb_sq[n] == x && bomb_sq[n+1] == y)
+				{
+					for (int i=0; i<mines_amount * 2; i+=2)
+					{
+						int w=bomb_sq[i];
+						int h=bomb_sq[i+1];
+						selected_squares[h][w]= '@';
+					}
+					selected_squares[y][x] = '!';
+					return true;
+				}
+			}
+			selected_squares[y][x]= '.';
+			set_score();
+			return false;
 		}
 };
 			
